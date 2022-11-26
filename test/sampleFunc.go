8600c60d2d144b27493b5e456a3992244d17dfb1
Barti <bartosz.majkowski@wp.pl>
2022-11-26 19:09:44 +0100

    automatic testing check

--- test/sampleFunc.go
+++ test/sampleFunc.go
@@ -3,3 +3,7 @@ package sampleTest
 func Sum(a int, b int) int {
 	return a + b
 }
+
+func Multiply(a int, b int) int {
+	return a * b
+}
--- test/sampleFunc_test.go
+++ test/sampleFunc_test.go
@@ -8,3 +8,10 @@ func TestSum(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	total := Multiply(5, 5)
+	if total != 25 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+}
