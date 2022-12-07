4660ac2f0201e0a18f7c13a7c4cc417f1e66dea8
piotrselak <piotr.selak.it@gmail.com>
2022-12-07 10:24:21 +0100

    init

---
+++ internal/schedule/repository/repository.go
@@ -0,0 +1,7 @@
+package repository
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+func FetchPlanForYear(collection *mongo.Collection, year string) interface{} { // idk if pointer is good here
+
+}
