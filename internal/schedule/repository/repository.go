aef73fd1940762b0a1e887ad44f0352056d12433
piotrselak <piotr.selak.it@gmail.com>
2022-12-08 14:30:04 +0100

    Progress

--- internal/schedule/repository/repository.go
+++ internal/schedule/repository/repository.go
@@ -1,7 +1,27 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"github.com/UniversityOfGdanskProjects/plan_back/internal/schedule/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-func FetchPlanForYear(collection *mongo.Collection, year string) interface{} { // idk if pointer is good here
+func FetchPlanForUser(coll *mongo.Collection,
+	yearID string, groupID string, userID string) (domain.Schedule, error) {
 
+	var result domain.Schedule
+	filter := bson.D{}
+
+	cursor, err := coll.Find(context.TODO(), filter) // ! but change this context
+	if err != nil {
+		return domain.Schedule{}, err
+	}
+
+	err = cursor.Decode(&result)
+	if err != nil {
+		return domain.Schedule{}, err
+	}
+
+	return result, nil
 }
