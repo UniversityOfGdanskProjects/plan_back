37f27edf0c17274a132d1f6f0e24f63cc180368b
piotrselak <piotr.selak.it@gmail.com>
2022-12-09 13:14:08 +0100

    test

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/api", HelloServer)
-    http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/api", HelloServer)
+	http.ListenAndServe(":8888", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello world")
+	fmt.Fprintf(w, "hello world")
 }
