0ee823eb4bf7651f3b430590e349b8580e02b281
Barti <bartosz.majkowski@wp.pl>
2022-12-13 21:23:55 +0100

    Struct and first test

--- internal/schedule/domain/schedule.go
+++ internal/schedule/domain/schedule.go
@@ -2,6 +2,13 @@ package domain
 
 type Schedule struct{}
 
-type GroupSchedule struct{}
+type YearSchedule struct {
+	yearID  string `validate:"required, numeric, len=4"`
+	groupID string `validate:"required, numeric, len=1"`
+}
 
-type YearSchedule struct{}
+type GroupSchedule struct {
+	yearID  string `validate:"required, numeric, len=4"`
+	groupID string `validate:"required, numeric, len=1"`
+	userID  string `validate:"required, numeric, len=5"`
+}
--- internal/schedule/domain/schedule_test.go
+++ internal/schedule/domain/schedule_test.go
@@ -1,1 +1,29 @@
 package domain
+
+import (
+	"github.com/go-playground/validator/v10"
+	"testing"
+)
+
+func TestYearSchedule(t *testing.T) {
+	yearSch := YearSchedule{
+		yearID:  "2001",
+		groupID: "4",
+	}
+	validate := validator.New()
+	err := validate.Struct(yearSch)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+//type YearSchedule struct {
+//	yearID  string `validate:"required, numeric, len=4"`
+//	groupID string `validate:"required, numeric, len=1"`
+//}
+//
+//type GroupSchedule struct {
+//	yearID  string `validate:"required, numeric, len=4"`
+//	groupID string `validate:"required, numeric, len=1"`
+//	userID  string `validate:"required, numeric, len=5"`
+//}
