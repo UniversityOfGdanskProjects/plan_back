2b9ed822c4342aa380282cbae55abf5ae60f392d
Barti <bartosz.majkowski@wp.pl>
2022-12-14 11:37:28 +0100

    added test and impoved domain validation

--- internal/schedule/domain/schedule.go
+++ internal/schedule/domain/schedule.go
@@ -3,12 +3,12 @@ package domain
 type Schedule struct{}
 
 type YearSchedule struct {
-	yearID  string `validate:"required, numeric, len=4"`
-	groupID string `validate:"required, numeric, len=1"`
+	YearID  string `json:"_id" validate:"required,numeric,len=4"`
+	groupID string `json:"group.id" validate:"required,numeric,len=1"`
 }
 
 type GroupSchedule struct {
-	yearID  string `validate:"required, numeric, len=4"`
-	groupID string `validate:"required, numeric, len=1"`
-	userID  string `validate:"required, numeric, len=5"`
+	yearID  string `json:"year_id" validate:"required,numeric,len=4"`
+	groupID string `json:"group_id" validate:"required,numeric,len=1"`
+	userID  string `json:"user_id" validate:"required"`
 }
--- internal/schedule/domain/schedule_test.go
+++ internal/schedule/domain/schedule_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestYearSchedule(t *testing.T) {
 	yearSch := YearSchedule{
-		yearID:  "2001",
-		groupID: "4",
+		YearID:  "2021",
+		groupID: "3",
 	}
 	validate := validator.New()
 	err := validate.Struct(yearSch)
@@ -17,13 +17,15 @@ func TestYearSchedule(t *testing.T) {
 	}
 }
 
-//type YearSchedule struct {
-//	yearID  string `validate:"required, numeric, len=4"`
-//	groupID string `validate:"required, numeric, len=1"`
-//}
-//
-//type GroupSchedule struct {
-//	yearID  string `validate:"required, numeric, len=4"`
-//	groupID string `validate:"required, numeric, len=1"`
-//	userID  string `validate:"required, numeric, len=5"`
-//}
+func TestGroupSchedule(t *testing.T) {
+	groupSch := GroupSchedule{
+		yearID:  "2022",
+		groupID: "4",
+		userID:  "hbsduab",
+	}
+	validate := validator.New()
+	err := validate.Struct(groupSch)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
